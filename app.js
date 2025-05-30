8983d6af1a34b6dd9d96ac5bed7d434a2cc86362
nilimamishra2002 <nilimamishra2002@gmail.com>
2025-05-30 19:40:08 +0530

    add project files

--- app.js
+++ app.js
@@ -11,7 +11,8 @@ const mongoose = require('mongoose');
 const MongoStore = require('connect-mongo');
 
 const User = require("./models/user.js");
-// const User = require("./models/medicine.js");
+const User = require("./models/medicine.js");
+
 const indexRouter = require("./routes/index.js");
 const userRouter = require("./routes/user.js");
 const dashboardRoutes = require('./routes/dashboard');
